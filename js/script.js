f29d8cd439e1a31f24fe9b1cb04bf261ca94df4b
Brent Lewis <brentdotlewis@gmail.com>
2018-08-30 22:55:19 -0500

    printQuote function is outputting random quotes to webpage. Added more quotes.

--- js/script.js
+++ js/script.js
@@ -22,6 +22,18 @@ var quotes = [
   {
     quote: "If you can dream it you can do it.",
     source: "Walt Disney"
+  },
+  {
+    quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    source: "Winston Churchill"
+  },
+  {
+    quote: "Everything you’ve ever wanted is on the other side of fear.",
+    source: "George Addair"
+  },
+  {
+    quote: "There is only one thing that makes a dream impossible to achieve: the fear of failure.",
+    source: "Paulo Coelho"
   }
 ];
 
@@ -31,18 +43,18 @@ function getRandomQuote(quotes) {
   var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   return randomQuote;
 }
-console.log ( getRandomQuote(quotes) );
 
-/*
+
 // Create the printQuote funtion and name it printQuote
 
-function print(quotes) {
-  document.getElementById('loadQuote');
+function printQuote () {
+  var selectedQuote = getRandomQuote(quotes);
+  var outputString = '<p class="quote">' + selectedQuote.quote + '</p>';
+      outputString += '<p class="source">' + selectedQuote.source + '</p>';
+  document.getElementById("quote-box").innerHTML = outputString;
 }
 
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-*/

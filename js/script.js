7980e5baaa351cef791faa3b274981031af6db11
Brent Lewis <brentdotlewis@gmail.com>
2018-09-03 19:53:13 -0500

    Added comments and final touches

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // FSJS - Random Quote Generator
 
+// Auto refresh quote after 10 seconds
+
+var quoteRefresh = setInterval(printQuote, 10000);
+var backgoundRefresh = setInterval(random_bg_color, 10000);
+
 // Create the array of quote objects and name it quotes
 
 var quotes = [
@@ -11,26 +16,29 @@ var quotes = [
     tag: "Motovational"
   },
   {
-    quote: "Have no fear of perfection, you'll never reach it.",
-    source: "Salvador Dali",
-    citation: "World wide web",
-    year: "2018",
+    quote: "We will not tire, we will not falter, and we will not fail.",
+    source: "George W. Bush",
+    citation: "Speach after 9/11",
+    year: "2001",
     tag: "Motovational"
   },
   {
-    quote: "It always seems impossible until it's done.",
-    source: "Nelson Mandela",
-    tag: "Motovational"
+    quote: "We must never forget that art is not a form of propaganda; it is a form of truth.",
+    source: "John F. Kennedy",
+    tag: "Art",
+    year: "October 26, 1963"
   },
   {
-    quote: "Be the change you want to see in the world.",
-    source: "Mahatma Gandhi",
-    tag: "Motovational"
+    quote: "I look foreword to an America which will not be afraid of grace and beauty.",
+    source: "John F. Kennedy",
+    tag: "Beauty",
+    year: "October 26, 1963"
   },
   {
-    quote: "If you can dream it you can do it.",
-    source: "Walt Disney",
-    tag: "Motovational"
+    quote: "I submit to you that if a man hasn't discovered something he will die for, he isn't fit to live.",
+    source: "Martin Luther King Jr.",
+    tag: "Speech in Detroit",
+    year: "June 23, 1963"
   },
   {
     quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
@@ -45,6 +53,16 @@ var quotes = [
   {
     quote: "There is only one thing that makes a dream impossible to achieve: the fear of failure.",
     source: "Paulo Coelho",
+    tag: "Motovational"
+  },
+  {
+    quote: "Always bear in mind that your own resolution to succeed is more important than any one thing.",
+    source: "Abraham Lincoln",
+    tag: "Success"
+  },
+  {
+    quote: "The future belongs to those who prepare for it today.",
+    source: "Malcolm X",
     tag: "Motovational"
   }
 ];
@@ -83,6 +101,7 @@ function random_bg_color() {
   var z = Math.floor(Math.random() * 256);
   var bgColor = "rgb(" + x + "," + y + "," + z + ")";
     document.body.style.background = bgColor;
+    // Gives button the same color as the random background 
     document.getElementById('loadQuote').style.backgroundColor = bgColor;
   }
 
@@ -94,21 +113,6 @@ function clickHandler() {
   printQuote();
 }
 document.getElementById('loadQuote').addEventListener('click', clickHandler, false);
-
-// Auto refresh quote after 3 seconds
-
-function autoRefresh() {
-  var quoteRefresh = setInterval(printQuote, 3000);
-  var backgoundRefresh = setInterval(random_bg_color, 3000);
-}
-autoRefresh()
-
-function stopRefresh() {
-    clearTimeout(quoteRefresh);
-    clearTimeout(backgoundRefresh);
-}
-stopRefresh()
-
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
